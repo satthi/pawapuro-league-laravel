d867a44f0f282d01d6f5eb94798db243abfc1ad2
Satoru Hagiwara <hagiwara@fusic.co.jp>
2022-03-04 15:46:42 +0000

    backup調整

--- backup.sh
+++ backup.sh
@@ -3,4 +3,9 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 cd $SCRIPT_DIR
 youbi=`date +%u`
 /usr/local/bin/docker-compose exec -T db /usr/local/bin/pg_dump -U postgres default > /tmp/backup.sql 2> /tmp/error.log
+bkfilesize=`wc -c /tmp/backup.sql | awk '{print $1}'`
+if [ $bkfilesize != 0 ] ; then
 sudo mv /tmp/backup.sql /vagrant/Dropbox/game2_{$youbi}.sql
+else
+echo 'error'
+fi
